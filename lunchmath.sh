1b651c0a3e4078040010497cdabdbb735ef83d92
Ubuntu <ubuntu@ip-172-31-43-81.ec2.internal>
2020-02-28 01:50:41 +0000

    Add lunchmath.sh, script on plotting lunch and math percentage

---
+++ lunchmath.sh
@@ -0,0 +1,9 @@
+set output 'lunchMath.png'
+
+set terminal 'png'
+
+set xlabel "LunchProgram"
+set ylabel "Math10Score"
+set title "% free lunch program v. %  pass MEAP math"
+plot passMathPercentage
+
